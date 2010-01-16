b7221900137c4cc71f4973303b814bf332447e71
jjhatf02 <jjhatf02@e1e1013e-0166-11df-b7b4-3d6a74a08a84>
2010-01-16 05:21:56 +0000

    Updated the database schema to match the enhanced structure that is utilized in the application.

--- trunk/database.sql
+++ trunk/database.sql
@@ -31,25 +31,44 @@ CREATE TABLE `contract` (
   `assassin` int(10) unsigned NOT NULL,
   `target` int(10) unsigned NOT NULL,
   `assigned` datetime NOT NULL,
-  `expireation` datetime NOT NULL,
+  `expiration` datetime NOT NULL,
   `updated` datetime NOT NULL,
   `status` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_contract_1` (`assassin`),
   KEY `FK_contract_2` (`target`),
   KEY `FK_contract_3` (`status`),
-  CONSTRAINT `FK_contract_3` FOREIGN KEY (`status`) REFERENCES `status` (`id`),
   CONSTRAINT `FK_contract_1` FOREIGN KEY (`assassin`) REFERENCES `users` (`id`),
-  CONSTRAINT `FK_contract_2` FOREIGN KEY (`target`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `FK_contract_2` FOREIGN KEY (`target`) REFERENCES `users` (`id`),
+  CONSTRAINT `FK_contract_3` FOREIGN KEY (`status`) REFERENCES `status` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
 
 --
--- Dumping data for table `contract`
+-- Definition of table `mail`
 --
 
-/*!40000 ALTER TABLE `contract` DISABLE KEYS */;
-/*!40000 ALTER TABLE `contract` ENABLE KEYS */;
+DROP TABLE IF EXISTS `mail`;
+CREATE TABLE `mail` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `to` varchar(45) NOT NULL,
+  `subject` varchar(45) NOT NULL,
+  `body` text NOT NULL,
+  `when` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
+--
+-- Definition of table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `message` text NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 --
 -- Definition of table `status`
@@ -60,13 +79,19 @@ CREATE TABLE `status` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `value` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `status`
 --
 
 /*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` (`id`,`value`) VALUES 
+ (1,'Open'),
+ (2,'Succeeded'),
+ (3,'Expired'),
+ (4,'Failed'),
+ (5,'Missed');
 /*!40000 ALTER TABLE `status` ENABLE KEYS */;
 
 
@@ -84,16 +109,38 @@ CREATE TABLE `users` (
   `phone` varchar(45) NOT NULL,
   `secret` varchar(10) NOT NULL,
   `active` tinyint(1) NOT NULL,
+  `spawn` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Definition of table `zeep_mo`
 --
 
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+DROP TABLE IF EXISTS `zeep_mo`;
+CREATE TABLE `zeep_mo` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(10) unsigned NOT NULL,
+  `body` varchar(255) NOT NULL,
+  `return` varchar(255) NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
+
+--
+-- Definition of table `zeep_sub`
+--
+
+DROP TABLE IF EXISTS `zeep_sub`;
+CREATE TABLE `zeep_sub` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(10) unsigned NOT NULL,
+  `min` varchar(255) NOT NULL,
+  `return` varchar(255) NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
 
